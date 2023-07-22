f401a57d501a9673e513fc4eab9371bb447554ae
DjAndrija <137707799+DjAndrija@users.noreply.github.com>
2023-07-23 01:04:26 +0200

    a
    aaaaaa

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -10,7 +10,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const { email, naslov, text, ime } = req.body;
+    const { email, naslov, text, ime, tip, date, broj } = req.body;
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -22,8 +22,10 @@ export default async function handler(
       from: email,
       to: "andrijadj96@gmail.com",
       subject: naslov,
-      html: `<h1>${ime}</h1>
+      html: `<h1>${ime} ${tip}</h1>
       <p>${text}</p>
+      <p>${date}</p>
+      <p>${broj} ljudi</p>
       from <a href="mailto:${email}">${email}</a>
       `,
     };
@@ -31,10 +33,40 @@ export default async function handler(
       from: "andrijadj96@gmail.com",
       to: email,
       subject: naslov,
-      html: `<h1>Poštovani, primili smo vašu poruku</h1>
-      <p>Poštovani, želimo da Vas obavestimo da smo primili Vašu poruku i da ćemo u najkraćem
-      mogućem roku odgovoriti na nju</p>
-      <h2>Srdačno, Vaš Pound </h2>
+      html: `  <body style="background-color: #ffcfa6; width: 100%">
+      <h1
+        id="pound"
+        style="
+          font-size: 10vh;
+          top: 5%;
+          left: 50%;
+          transform: translateX(-50%);
+          position: absolute;
+          margin: 0;
+          margin-bottom: 5%;
+          letter-spacing: 1vw;
+          filter: drop-shadow(0.4vw -0.4vw 0px #ff8870);
+        "
+      >
+        POUND
+      </h1>
+      <img src="../../public/logo.svg" alt="" id="logo" />
+      <h1
+        style="
+          width: 50%;
+          text-align: center;
+  
+          margin-block: 15% 2.5%;
+        "
+      >
+        Po<span>š</span>tovani,
+      </h1>
+      <p>
+        Primili smo va<span>š</span>u poruku, i <span>ž</span>elimo da vas
+        obavestimo da <span>ć</span>emo u najkra<span>ć</span>em roku da
+        odgovorimo na nju!
+      </p>
+    </body> 
       `,
     };
     try {
